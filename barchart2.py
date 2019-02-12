174f3e80e31a89e3ecb532027e9dc664fb2f8d07
miltonjdaz <47509499+miltonjdaz@users.noreply.github.com>
2019-02-12 17:29:28 -0600

    added red bars & changed to sum

--- barchart2.py
+++ barchart2.py
@@ -11,18 +11,18 @@ url = 'http://people.terry.uga.edu/rwatson/data/manheim.txt'
 data = pd.read_csv(url)
 
 # y values - numbers
-xmean = data.loc[data['model']=='X', 'price'].mean()
-ymean = data.loc[data['model']=='Y', 'price'].mean()
-zmean = data.loc[data['model']=='Z', 'price'].mean()
-yvals = [xmean, ymean, zmean]
+xsum = data.loc[data['model']=='X', 'price'].sum()
+ysum = data.loc[data['model']=='Y', 'price'].sum()
+zsum = data.loc[data['model']=='Z', 'price'].sum()
+yvals = [xsum, ysum, zsum]
 
 # x values - model names
 model_list = data.model.unique()
 model_list.sort()
 
 # add it all on to the blank canvas to plot
-plt.bar(model_list, yvals, align='center', alpha=0.5)
+plt.bar(model_list, yvals, align='center', alpha=0.5, color = 'red')
 plt.xlabel("model")
 plt.ylabel("price")
-plt.title("Average Prices for Each Model")
+plt.title("Sum Prices for Each Model")
 plt.show()
