8f6a15414f88fdfb7e1e275d7453800dd234b0dd
miltonjdaz <47509499+miltonjdaz@users.noreply.github.com>
2019-02-17 15:41:58 -0600

    average.py

--- single_scripts/average.py
+++ single_scripts/average.py
@@ -7,7 +7,7 @@
 def main():
 	n = int(input("How many numbers do you want to add? "))
 	sum = 0 
-	for n in range(n):
+	for i in range(n):
 		value = int(input("Enter next num: "))
 		sum = sum + value
 		average = round(sum/n, 2)
