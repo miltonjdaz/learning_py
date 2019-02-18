68ac4d375a7d30315fe783bb2e0870056fa75f21
miltonjdaz <47509499+miltonjdaz@users.noreply.github.com>
2019-02-18 14:31:25 -0600

    km_to_m.py

---
+++ single_scripts/km_to_m.py
@@ -0,0 +1,10 @@
+# Milton D'az #
+# File: km_to_m.py
+
+# A program that converts kilometers to miles
+
+def main ():
+	kilometers = eval(input("How many kilometers? ")) 
+	miles = kilometers * 0.62137  
+	print ("The kilometers converted to miles is", miles , "miles.")
+main()
