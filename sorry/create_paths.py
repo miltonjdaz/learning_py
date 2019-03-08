826b137c17a6025b285af6f856001d4f08763cbd
Angel <angelddaz@gmail.com>
2019-03-07 20:59:10 -0700

    finished writing creat_paths file which is called in main.py, to create 4 dicts/paths

---
+++ sorry/create_paths.py
@@ -0,0 +1,76 @@
+"""
+This file creates 4 dictionaries which define the paths for each of the four players.
+
+Assuming a player is facing up, the path follows this amount of steps.
+    # adding up the spots for path. It is 55 regular + 6 final
+    # 5 up 
+    # 1 left
+    # 1 up
+    # 5 left
+    # 2 up
+    # 5 right
+    # 1 up
+    # 1 right
+    # 5 up
+    # 2 right
+    # 5 down
+    # 1 right
+    # 1 down
+    # 5 right
+    # 2 down
+    # 5 left
+    # 1 down
+    # 1 left
+    # 5 down
+    # 1 left
+    # 6 up
+"""
+def main():
+    paths=[]
+    for i in range(0,4):
+        path={}
+
+
+        # TODO you can add 14 and append to spawn_points 
+        # in each i for the outer for loop
+        # spawn points append spawn_points[i-1]+14
+        spawn_points=[0,14,28,42]
+        # there are 14 spots between each spawn point
+        # p2: 14 - 55 then 0 - 13 then the last 6 spots for p2
+        # and so on for p3 and p4.
+
+
+        # adding 55 to each value in spawn_points to get a ending point before the final 6 spots
+        # ex: new_list = [x+1 for x in my_list]
+        ending_points=[x+55 for x in spawn_points]
+        custom_range=range(spawn_points[i],ending_points[i])
+
+        for j in custom_range:
+            if i == 0 or j < 55:
+                # player one needs no number manipulation
+                key_j='spot_{}'.format(j)
+                path[key_j]=None
+            # around to 0 after reaching 55
+            # players 2,3, and 4 need their path to loop back 
+            elif j >= 55:
+                # in order to loop back around from 54 to 0
+                # here, we format the key string to use j minus
+                # the last number of the first i=0 loop custom_range
+
+                # for example, once j gets to 55 on i=1, we want the key
+                # string to say 0, so we subtract 55 AKA ending_points[0] from j
+                key_j='spot_{}'.format(j-ending_points[0])
+                path[key_j]=None
+
+        final_spots_range=range(0,7)
+        for k in final_spots_range:
+            key_k = 'p{0}_final_spot_{1}'.format(i+1,k)
+            path[key_k]=None
+        paths.append(path)
+    return paths
+
+
+        
+
+
+    
--- sorry/main.py
+++ sorry/main.py
@@ -1,104 +1,21 @@
 import random
+import create_paths
 
 p1 = ["c1", "c2", "c3", "c4"]
 p2 = ["c1", "c2", "c3", "c4"]
 p3 = ["c1", "c2", "c3", "c4"]
 p4 = ["c1", "c2", "c3", "c4"]
 
-dice_min = 1
-dice_max = 6
+dice_min=1
+dice_max=6
 
-roll_result = random.randint(dice_min, dice_max)
+roll_result=random.randint(dice_min, dice_max)
 
-"""
-res = {}
-for j in xrange(10):
-    key_j = 'key_{}'.format(j)  # a string depending on j
-    res[key_j] = j**2
-"""
-pathcounter = 55 
-# TODO create paths for all players
-# while loop. While the counter is < 55, starting from 0, add up the index
-while pathcounter is < 55:
-    p2_path = {}
-        for i in range(14, 13): 
-            key_i = 'spot_{}'.format(i)
-            p2_path[key_i]=None
-            print(key_i)
+# this returns 4 dictionaries for 4 player paths from another file
+pathlist=create_paths.main()
 
-        for i in range(0,7):
-            key_i = 'p2_final_spot_{}'.format(i)
-            p1_path[key_i]=None #f for final. f1 for final1
-            print(key_i)
-        print(p2_path)
-
-    p3_path = {}
-        for i in range(28, 27): 
-            key_i = 'spot_{}'.format(i)
-            p3_path[key_i]=None
-            print(key_i)
-
-        for i in range(0,7):
-            key_i = 'p3_final_spot_{}'.format(i)
-            p1_path[key_i]=None #f for final. f1 for final1
-            print(key_i)
-        print(p3_path)
-        
-    p4_path = {}
-        for i in range(42, 41): 
-            key_i = 'spot_{}'.format(i)
-            p4_path[key_i]=None
-            print(key_i)
-
-        for i in range(0,7):
-            key_i = 'p4_final_spot_{}'.format(i)
-            p1_path[key_i]=None #f for final. f1 for final1
-            print(key_i)
-        print(p4_path)
-else: NONE 
-
-# p2: 14 - 55 then 0 - 13 then p2final path
-# and so on for p3 and p4.
-
-# adding up the spots for path. It is 55 regular + 6 final
-p1_path = {}
-
-# these are regular path
-for i in range(0,55): 
-    key_i = 'spot_{}'.format(i)
-    p1_path[key_i]=None
-    print(key_i)
-
-for i in range(0,7):
-    key_i = 'p1_final_spot_{}'.format(i)
-    p1_path[key_i]=None #f for final. f1 for final1
-    print(key_i)
-print(p1_path)
-
-# 5 up 
-# 1 left
-# 1 up
-# 5 left
-# 2 up
-# 5 right
-# 1 up
-# 1 right
-# 5 up
-# 2 right
-# 5 down
-# 1 right
-# 1 down
-# 5 right
-# 2 down
-# 5 left
-# 1 down
-# 1 left
-# 5 down
-# 1 left
-# 6 up
-
-
-
+# to see player 4 path:
+print(pathlist[3])
 # TODO print out board from data structures
 
 # TODO create movement method which depends on dice rolls
