4c248155a524533dfa51dd9fea920d535b630d9a
Angel <angelddaz@gmail.com>
2019-03-31 17:16:00 -0600

    satisfied more decision branches and started user input branches

--- sorry/movement.py
+++ sorry/movement.py
@@ -22,46 +22,113 @@ p1_finished_coins_counter=0
 p2_finished_coins_counter=0
 p3_finished_coins_counter=0
 
-if(player==0):
-    coins_to_move=p0
-elif(player==1):
-    coins_to_move=p1
-elif(player==2):
-    coins_to_move=p2
-else:
-    coins_to_move=p3
+def set_coins(player):
+    if(player==0):
+        return p0
+    elif(player==1):
+        return p1
+    elif(player==2):
+        return p2
+    else:
+        return p3
+
+coins_to_move=set_coins(player)
+
 
 # spawning the first player since they were the first to roll a six in main.py
 path_to_move_in=four_paths[player]
 first_spot=str(next(iter(path_to_move_in.keys())))
 path_to_move_in[first_spot]=coins_to_move[0]
+print("Player {} has rolled the first 6 and spawned.".format(player))
+
+
+
+def path_is_empty(path_to_move_in):
+    if(all(value is None for value in path_to_move_in.values())):
+        return True
+    else:
+        return False
+
+# this function moves the furthest coin in a dictionary forward the amount passed into the first
+# parameter which is called, rolled.
+def move_forward(rolled, path_to_move_in):
+    # we check how many coins are on the board
+    coin_count=0
+    coins_on_board=[]
+    coin_to_move=""
+    movement_counter=0
+
+    for k, v in path_to_move_in.items():
+        if v!=None:
+            coin_count+=1
+            # TODO use following line's list to check against possible killing of other player coins
+            coins_on_board.append(k) # keeping a list of dictionary keys with coins in them for single player
+    
+    # TODO Allow player to choose which coin to move. 
+    # Currently assuming they want to move the furthest
+    # which is = to rolled+(previous)furthest coin key
+    furthest_coin_key=coins_on_board[-1]
+
+    for k, v in path_to_move_in.items():
+        # once we finally come across the furthest coin
+        # we store in coin_to_move var and empty out the dict value (v)
+        if k==furthest_coin_key:
+            coin_to_move=v
+            path_to_move_in[k]=None
+        else:
+            v=v # trivial line which explains that nothing happens
+        # if we already moved the coin out to be moved forward
+        if path_to_move_in[furthest_coin_key]==None and movement_counter<rolled:
+            movement_counter+=1
+        # if we have looped around enough times to catch the rolled amount
+        elif movement_counter==rolled:
+            # TODO check if movement will run out of spots and then move next furthest if so
+            path_to_move_in[k]=coin_to_move
+            rolled=0 # breaks out of these conditionals because movement counter will never equal 0
+        else:
+            path_to_move_in[k]=path_to_move_in[k]
+    return path_to_move_in
+
+def roll(player):
+    roll_result=random.randint(dice_min, dice_max)
+    return(roll_result)
+
+def spawn(path_to_move_in, coins_to_move):
+    # TODO Check if this spot is empty first, to see if an option to kill exists.
+    first_spot=str(next(iter(path_to_move_in.keys())))
+    flag=0
+    for i in range(0,4):
+        if coins_to_move[i]!=None and flag==0: # checks for very first coin that is not empty in coin list for player
+            path_to_move_in[first_spot]=coins_to_move[i]
+            coins_to_move[i]=None # coin has moved to the board/path/dictionary
+            print("Player {} has rolled a {} with an empty board and spawned.".format(player, rolled))
+            flag=1 # forces conditional to only be used once.
+    return path_to_move_in, coins_to_move
 
 # we want to roll over and over again for each player and handle each player's roll result differently, depending on their board situation
-
-
 # while a player has not won
-while(p0_finished_coins_counter!=4 and p1_finished_coins_counter!=4 and p2_finished_coins_counter!=4 and p3_finished_coins_counter!=4):
+loops_to_test=30
+while(p0_finished_coins_counter!=loops_to_test and p1_finished_coins_counter!=loops_to_test and p2_finished_coins_counter!=loops_to_test and p3_finished_coins_counter!=loops_to_test):
 
-    def roll(player):
-        roll_result=random.randint(dice_min, dice_max)
-        return(player, roll_result)
-
+    # resetting the player, amount rolled, and coins to move at the beginning of every while loop
     path_to_move_in=four_paths[player]
-    player, rolled=roll(player)
+    rolled=roll(player)
+    coins_to_move=set_coins(player)
 
     if(rolled==6):
         # if our player's path is completely empty, we want to just spawn in one coin
-        if(all(value is None for value in path_to_move_in.values())):
-            first_spot=str(next(iter(path_to_move_in.keys())))
-            path_to_move_in[first_spot]=coins_to_move[0]
-            coins_to_move[0]=None # coin has moved to the board/path/dictionary
+        if(path_is_empty(path_to_move_in)==True):
+            path_to_move_in, coins_to_move=spawn(path_to_move_in, coins_to_move)
         # if player rolled a six and path is not empty
-        # then we check for how many coins are on the path
         else:
-            coin_count=0
-            for i in path_to_move_in.values():
-                if isinstance(path_to_move_in[i], list):
-                    count+=len(path_to_move_in[i])
+            print("Congrats! Player {} has rolled a 6 and already has at least one coin on the board.".format(player))
+            print("Would you like to spawn a new coin? If no, your furthest coin moves ahead.")
+            answer=input("Y/N: ")
+            if(answer=="y" or answer=="Y"):
+                path_to_move_in, coins_to_move=spawn(path_to_move_in, coins_to_move)
+            elif(answer=="n" or answer=="N"):
+                # move a coin - do not spawn new one
+                path_to_move_in=move_forward(rolled, path_to_move_in)
         
         player=player # a trivial line to show that rolling a 6, lets the same player roll again
 
@@ -73,35 +140,11 @@ while(p0_finished_coins_counter!=4 and p1_finished_coins_counter!=4 and p2_finis
     # TODO once a player does not roll a 6, player ++ to move on to the next player
     else: # if player did not roll a 6
         # if the board is empty, nothing happens here
-        if(all(value is None for value in path_to_move_in.values())):
-            print("Player {} rolled a {}, but the board is empty.".format(player+1, rolled))
+        if(path_is_empty(path_to_move_in)==True):
+            print("Player {} rolled a {}, but the board is empty.".format(player, rolled))
         else: # if board is not empty and player rolled != 6
+            path_to_move_in=move_forward(rolled, path_to_move_in)
             
-            # we check how many coins are on the board
-            coin_count=0
-            coins_on_board=[]
-            for k, v in path_to_move_in.items():
-                if v!=None:
-                    coin_count+=1
-                    # TODO use following line's list to check against possible killing of other player coins
-                    coins_on_board.append(k) # keeping a list of dictionary keys with coins in them for single player
-                
-            # TODO allow the player to choose which coin to move ahead
-            # for now, we force player to move furthest coin disregarding eveything else
-            furthest_coin_key=coins_on_board[-1]
-            furthest_coin_value=path_to_move_in[furthest_coin_key]
-            path_to_move_in[furthest_coin_key]=None # empty out the furthest spot
-
-            # TODO move furthest coin value into a new furthest_coin_key 
-            # which is = to rolled+(previous)furthest coin key
-            # TODO using this for loop: for k, v in first_path.items()
-            # we will check for when a key is equal to the furthest coin key
-            # if it is, we move start a counter and once the counter = amount rolled
-            # then we stick the value in this new, further long, key
-            
-            # we also need to check for final spots when moving forward.
-
-
         # circling players from 0 - 3
         if player > 2:
             player=0
@@ -109,9 +152,13 @@ while(p0_finished_coins_counter!=4 and p1_finished_coins_counter!=4 and p2_finis
             player+=1
             
     # TODO a temporary break to the loop
-    print("a player won. not really")
     p0_finished_coins_counter+=1
-    
 
 
-
+# print(four_paths[0])
+# print("")
+# print(four_paths[1])
+# print("")
+# print(four_paths[2])
+# print("")
+# print(four_paths[3])
--- sorry/testing_dicts.py
+++ sorry/testing_dicts.py
@@ -2,30 +2,53 @@ import create_paths
 # this script checks out how to loop dictionaries and how we can use the string keys and values in movement.py
 
 four_paths=create_paths.main()
-first_path=four_paths[0]
+path_to_move_in=four_paths[0]
 
-first_spot=str(next(iter(first_path.keys())))
-first_path[first_spot]="coin0"
+first_spot=str(next(iter(path_to_move_in.keys())))
+path_to_move_in[first_spot]="coin0"
 
-first_path["spot_49"]="coin1"
-rolled=2 
+path_to_move_in["spot_49"]="coin1"
+rolled=6
 
 coin_count=0
 coins_on_board=[]
-for k, v in first_path.items():
+for k, v in path_to_move_in.items():
     if v!=None:
         coin_count+=1
         coins_on_board.append(k) # keeping a list of dictionary keys with coins in them for single player
 # print(coint_count)
 
 furthest_coin_key=coins_on_board[-1]
-print(furthest_coin_key)
+coin_to_move=""
+movement_counter=0
+for k, v in path_to_move_in.items():
+	# once we finally come across the furthest coin
+	# we store in coin_to_move var and empty out the dict value (v)
+	if k==furthest_coin_key:
+		print(k, v)
+		coin_to_move=v
+		path_to_move_in[k]=None
+	else:
+		v=v # trivial line which explains that nothing happens
+	# if we already moved the coin out to be moved forward
+	if path_to_move_in[furthest_coin_key]==None and movement_counter<rolled:
+		movement_counter+=1
+		print(movement_counter)
+	# if we have looped around enough times to catch the rolled amount
+	elif movement_counter==rolled:
+		path_to_move_in[k]=coin_to_move
+		rolled=0 # breaks out of these conditionals because movement counter will never equal 0
+	else:
+		path_to_move_in[k]=path_to_move_in[k]
+print(path_to_move_in)
 
-furthest_coin_value=first_path[furthest_coin_key]
-print(furthest_coin_value)
 
+# this function should be working correctly
+path_to_move_in=four_paths[1]
+def path_is_empty(path_to_move_in):
+    if(all(value is None for value in path_to_move_in.values())):
+        return True
+    else:
+        return False
 
-
-first_path[furthest_coin_key]=None # empty out the furthest spot
-print(first_path[furthest_coin_key])
-# str(next(iter(path_to_move_in.keys())))
+print(path_is_empty(path_to_move_in))
