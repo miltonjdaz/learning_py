474d73312ffe10fda75dcf8e900a250db425a0d0
miltonjdaz <miltonjdaz@yahoo.com>
2019-11-21 12:20:38 -0600

    getting started on textbook

---
+++ textbook/Chapter1.py
@@ -0,0 +1,15 @@
+#right click white area, open in terminal, type code (Fill in blank).py
+
+# Basic function
+def hello(): 
+    print("Hello World!")
+
+hello()
+
+# Basic parameter 
+
+def greet(person):
+    print("Hello", person)
+    print("How are you?")
+
+greet("John")
