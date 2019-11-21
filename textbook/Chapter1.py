9bb2cd2da6e466349d39ae3292f54ae89e32f954
miltonjdaz <miltonjdaz@yahoo.com>
2019-11-21 12:59:19 -0600

    Finished materials in chapter 1

--- textbook/Chapter1.py
+++ textbook/Chapter1.py
@@ -12,4 +12,14 @@ def greet(person):
     print("Hello", person)
     print("How are you?")
 
-greet("John")
+greet("Claire")
+
+# A simple program illustrating chaotic behavior
+
+def main(): 
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(10):
+        x = 3.9 * x* (1-x) 
+        print(x)
+main()
