3299f266607ff04f86517310ed42e67c541ea660
miltonjdaz <miltonjdaz@yahoo.com>
2019-11-27 10:21:05 -0600

    format: added comment to explain bash script

--- copy_dags.sh
+++ copy_dags.sh
@@ -1,2 +1,3 @@
+# kill and fill airflow_dags folder since symbolic link didn't work
 rm -rf /home/milton/github/learning_py/airflow_dags
 cp -rf /home/milton/airflow/dags /home/milton/github/learning_py/airflow_dags
