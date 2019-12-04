48ccf738d823ae3f59e50b88aab9151b050df726
miltonjdaz <miltonjdaz@yahoo.com>
2019-12-04 10:44:06 -0600

    Using more things from math

--- textbook/Ch3Exercises.py
+++ textbook/Ch3Exercises.py
@@ -18,3 +18,33 @@ def cal():
     print(average)
 
 cal()
+
+import math 
+
+def vanda(): 
+    r = int(input("What is the length of the radius: "))
+    V = (4/3)*(math.pi)*(r^3)
+    A =  (4)*(math.pi)*(r^2)
+    print("The volume of the sphere is:", V)
+    print("The surface area of the sphere is:", A)
+
+vanda() 
+
+def light(): 
+    sec = int(input("How many have seconds passed: "))
+    sos = 1100 * sec
+    miles = float(sos / 5280)
+    print("The amount of miles from the lightning strike is:", miles, "miles")
+
+light()
+
+
+def tri(): 
+    a = int(input("The length of side a: "))
+    b = int(input("The length of side b: "))
+    c = int(input("The length of side c: "))
+    s = (a + b + c) / 2 
+    A = float(math.sqrt(s*(s-a)*(s-b)*(s-c)))
+    print("The area of a triable is:", A)
+
+tri()
