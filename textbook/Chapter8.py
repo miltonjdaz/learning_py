2a502f72730563339e208d5014362dc26d374019
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-10 09:52:03 -0600

    backgrounds and files

--- textbook/Chapter8.py
+++ textbook/Chapter8.py
@@ -74,3 +74,45 @@ def mainsix():
     print("\nThe average of the numbers is", total / count)
 
 mainsix()
+
+def mainsev():
+    fileName = input("What file are the numbers in? ")
+    infile = open(fileName, 'r')
+    total = 0.0
+    count = 0 
+    line = infile.readline()
+    while line != "":
+        # update total and count for values in line
+        for xStr in line.split(","):
+            total = total + float(xStr)
+            count = count + 1
+        line = infile.readline()
+    print("\nThe average of the numbers is" , total / count)
+
+mainsev() 
+
+# keyboard driven color changing window
+
+from graphics import * 
+
+def color():
+    win = GraphWin("Color Window", 500, 500)
+    # event loop: handle key presses until user presses the "q" key. 
+    while True:
+        key = win.getKey()
+        if key == "q": # loop exit
+            break
+        # process the key 
+        if key == "r": 
+            win.setBackground("pink")
+        elif key == "w":
+            win.setBackground("white")
+        elif key = "g": 
+            win.setBackground("lightgray")
+        elif key == "b":
+            win.setBackground("lightblue")
+
+    # exit program
+    win.close()
+
+color() 
