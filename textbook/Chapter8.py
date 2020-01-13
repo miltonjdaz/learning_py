c9f404878b610581df48165bc8c7ae65cbc6baac
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-13 10:40:21 -0600

    End of 8

--- textbook/Chapter8.py
+++ textbook/Chapter8.py
@@ -116,3 +116,36 @@ def color():
     win.close()
 
 color() 
+
+from graphics import * 
+
+def handleKey(k, win):
+    if k == "r":
+        win.setBackground("pink")
+    elif k == "w": 
+        win.setBackground("white")
+    elif k == "g": 
+        win.setBackground("lightgray")
+    elif k == "b":
+        win.setBackground("lightblue")
+
+def handleClick(pt, win): 
+    pass
+
+def mainf():
+    win = GraphWin("Click and Type", 500, 500)
+
+    # event key: handle key presses and mouse clicks until the user presses the "q" key
+
+    while True: 
+        key = win.checkKey()
+        if key == "q": # exit loop
+            break 
+        if key: 
+            handleKey(key, win)
+        pt = win.checkMouse()
+        if pt: 
+            handleClick(pt, win)
+    win.close()
+
+mainf() 
