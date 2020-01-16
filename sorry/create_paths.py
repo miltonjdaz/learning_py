e6b6afc1b1bc091eaa7509be07aa0bf1219da87c
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-16 09:04:33 -0600

    Path for the board game

--- sorry/create_paths.py
+++ sorry/create_paths.py
@@ -30,7 +30,9 @@ def main():
     # there are 14 spots between each spawn point
     # p2: 14 - 55 then 0 - 13 then the last 6 spots for p2
     # and so on for p3 and p4.
+    
     spawn_points=[]
+
     for i in range(0,4):
         if i == 0:
             spawn_points.append(0)
@@ -40,27 +42,35 @@ def main():
     ending_points=[x+55 for x in spawn_points]
 
     paths=[]
+    
     for i in range(0,4):
         path={}
         custom_range=range(spawn_points[i],ending_points[i])
         for j in custom_range:
             if i == 0 or j < 55:
+                
                 # player one needs no number manipulation
+                
                 key_j='spot_{}'.format(j)
                 path[key_j]=None
+            
             # around to 0 after reaching 55
             # players 2,3, and 4 need their path to loop back 
+            
             elif j >= 55:
+                
                 # in order to loop back around from 54 to 0
                 # here, we format the key string to use j minus
                 # the last number of the first i=0 loop custom_range
 
                 # for example, once j gets to 55 on i=1, we want the key
                 # string to say 0, so we subtract 55 AKA ending_points[0] from j
+                
                 key_j='spot_{}'.format(j-ending_points[0])
                 path[key_j]=None
 
         final_spots_range=range(0,6)
+        
         for k in final_spots_range:
             key_k = 'p{0}_final_spot_{1}'.format(i+1,k)
             path[key_k]=None
