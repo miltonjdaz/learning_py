7e6f8d3c8734cd18277e81d01578d4a6772073c8
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-16 09:01:16 -0600

    Coding is hard when your arms are super sore

---
+++ textbook/Review.py
@@ -0,0 +1,13 @@
+# A program asking someone how many numbers they want to average.
+ 
+def main():
+	n = int(input("How many numbers do you want to add? "))
+	sum = 0 
+	for i in range(n):
+		value = int(input("Enter next num: "))
+		sum = sum + value
+		average = round(sum/n, 2)
+	print()
+	print(average)
+
+main()
