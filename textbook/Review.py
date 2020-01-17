cac4c407ed9e0ac3b17f075c3e5a4b0381b02442
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-17 08:18:12 -0600

    de-coding messages

--- textbook/Review.py
+++ textbook/Review.py
@@ -11,3 +11,22 @@ def main():
 	print(average)
 
 main()
+
+# A program that de-codes a message.
+
+def main():
+    encodemsg = input("Enter the message to decrypt: ")
+    shift = int(input("Enter the amount of the shift: "))
+
+    encodemsg = encodemsg.split()
+    sentence = []
+
+    for word in encodemsg:
+        cipher_ords = [ord(x) for x in word]
+        plaintext_ords = [o - shift for o in cipher_ords]
+        plaintext_chars = [chr(i) for i in plaintext_ords]
+        plaintext = ''.join(plaintext_chars)
+        sentence.append(plaintext)
+    sentence = ' '.join(sentence)
+    print (sentence)
+main()
