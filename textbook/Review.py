99eafa7d8519855a5549c42b3a33d0eb6a40b25f
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-21 07:53:21 -0600

    Guessing a number

--- textbook/Review.py
+++ textbook/Review.py
@@ -107,3 +107,31 @@ def main():
 		print(lookup.get(x).pay())
 	print("==========================================")
 main()
+
+# A program that is a guessing game.
+
+import random
+
+def generate_number():
+	n = random.randint (1,100)
+	return n
+
+def main():
+	# input
+	try:
+		num_to_guess = generate_number()
+	#input from end-user 
+		guess = int(input("Enter a guess: "))
+		while guess != num_to_guess:
+			if guess < num_to_guess:
+				print("Your guess is too low")
+				guess = int(input("Enter a guess: "))
+			elif guess > num_to_guess:
+				print("Your guess is too high")
+				guess = int(input("Enter a guess: "))
+			else:
+				guess = get_guess() 
+		print("Winner chicken dinner!") 
+	except ValueError:
+		print("Please enter a number")
+main()
