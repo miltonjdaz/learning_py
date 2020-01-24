bc99b4943dfe1070940ed837b7f82f215c47de77
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-24 09:06:50 -0600

    GPA

--- textbook/Review.py
+++ textbook/Review.py
@@ -194,3 +194,18 @@ plt.xlabel("model")
 plt.ylabel("price")
 plt.title("Average Prices for Each Model")
 plt.show()
+
+# A program that finds the average of students GPA.
+
+def main():
+	total = 0.0
+	count = 0 
+	while True: 
+		number = float(input("Enter a positive number (-1 to quit): "))
+		if (number < 0):
+			break
+		total = total + number
+		count = count + 1
+	print("The average GPA is:", (round((total/count), 2)))
+	print("Number of students:", count)
+main()
