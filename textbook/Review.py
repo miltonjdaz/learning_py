ebcc9a2e0153451d1c219aac22058a7d20bbef12
miltonjdaz <miltonjdaz@yahoo.com>
2020-01-27 10:13:36 -0600

    calculating the ERA

--- textbook/Review.py
+++ textbook/Review.py
@@ -209,3 +209,16 @@ def main():
 	print("The average GPA is:", (round((total/count), 2)))
 	print("Number of students:", count)
 main()
+
+# A program calculating the ERA 
+
+def calculate_era(runs, innings):
+	era = (runs * 9)/innings
+	return era 
+def main():
+	n = int(input("How many pitchers: "))
+	for pitcher in range(n):
+		innings = int(input("How many innings: "))
+		runs = int(input("How many runs: "))
+		print (round(calculate_era(runs, innings), 2))
+main()
