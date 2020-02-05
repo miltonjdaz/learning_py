027794b7bd74c4ebc511f6ee6712233030837209
miltonjdaz <miltonjdaz@yahoo.com>
2020-02-05 09:07:58 -0600

    Reverse

--- textbook/Review.py
+++ textbook/Review.py
@@ -422,3 +422,20 @@ def main():
 	except:
 		print("You must enter a number.")
 main()
+
+# A program reversing the contents of a file.
+
+def main():
+	filename = input("Enter the filename: ")
+	infile = open (filename, "r")
+	data = infile.read()
+	print(data)
+	fn_rev = filename.split(".")
+	new_file = (fn_rev[0]+"_reverse."+fn_rev[1])
+	print(new_file)
+	opfile = open(new_file, "w")
+	data[::-1]
+	print(data[::-1], file=opfile)
+	opfile.close()
+	infile.close()
+main()
