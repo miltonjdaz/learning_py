78a4557d94aed420a81ce04ae50a4e0ebf5781e2
miltonjdaz <miltonjdaz@yahoo.com>
2020-02-11 10:04:39 -0600

    Changing str to int

--- textbook/Review.py
+++ textbook/Review.py
@@ -565,3 +565,21 @@ def main():
 	Celsius = round (((Fahrenheit - 32) * (5/9)), 2)
 	print ("The temperature is", Celsius, "degrees C")
 main() 
+
+# A program changing str to int
+
+def toNumbers(strList):
+	print("List after call:", [24,45])
+	for i in strList:
+		var = int(i)
+		print(var, "is a", type(var))
+	return var
+
+
+def main():
+	numbers = '24', '45'
+	print("List before call:", list(numbers))
+	print("24 is a", type ('24'))
+	print("45 is a", type ('45'))
+	toNumbers(numbers)
+main()
