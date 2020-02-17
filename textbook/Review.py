20a7ca74780052d2267d897fe93871d773e6e4f6
miltonjdaz <miltonjdaz@yahoo.com>
2020-02-17 10:46:38 -0600

    Contacts

--- textbook/Review.py
+++ textbook/Review.py
@@ -718,3 +718,17 @@ def main():
     message = chr(ord(ch) + key)
     print (message, end = "")
 main()
+
+# A program asking someone how many people they have in their contacts.
+ 
+def main():
+	n = int(input("How many people are in your contact list? "))
+	contacts = []
+	for i in range(n):
+		fname = (input("Enter the first name: "))
+		lname = (input("Enter the last name: "))
+		contact = str(i + 1) + ". " + lname + ", " + fname
+		contacts.append(contact)
+	for contact in contacts:
+		print(contact)
+main()
