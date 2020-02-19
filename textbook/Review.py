b72567c8a7a2d24aac4a0300d2d13a0a7cdfee07
miltonjdaz <miltonjdaz@yahoo.com>
2020-02-19 09:32:07 -0600

    Liters to gallons

--- textbook/Review.py
+++ textbook/Review.py
@@ -772,3 +772,15 @@ def main():
 	GPA = calc_gpa()
 	print("Your GPA is", GPA)
 main()
+
+# This program converts liters to gallons 
+
+print("The following is a converter of liters to gallons from 1 - 10") 
+
+def main():
+	for i in range(10): 
+		liters = i + 1
+		gallons = liters * 0.264172
+		print(liters,"\t",gallons)
+	input("Press the <Enter> key to quit.")
+main()
