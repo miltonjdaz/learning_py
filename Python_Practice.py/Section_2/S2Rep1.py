812e71b2f24e201403c8abe01c9f7193dc3d5047
miltonjdaz <miltonjdaz@yahoo.com>
2020-02-24 11:44:10 -0600

    Conversion program with intro

---
+++ Python_Practice.py/Section_2/S2Rep1.py
@@ -0,0 +1,12 @@
+""" Section 2 Rep 1 """
+
+# Temperature conversion with an introduction
+
+def main():
+    print("This program converts Celsius temperatures to Fahrenheit.")
+    print()
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9.0 / 5.0 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+main()
