1a8aba383b5ee90feeda8b36bfdb28f1be4ef660
miltonjdaz <miltonjdaz@yahoo.com>
2020-03-02 19:01:17 -0600

    Acronym program

---
+++ Python_Practice.py/Section_5/S5_Rep4.py
@@ -0,0 +1,15 @@
+""" Section 5 Rep 4 """
+
+# Acronym program 
+
+def main(): 
+
+    words = input("Please enter the words: ")
+    acronym = ""
+    for word in words.split(): 
+        acronym = acronym + word[]
+    acronym = acronym.upper 
+
+    print("The acronym is:", acronym)
+
+main() 
