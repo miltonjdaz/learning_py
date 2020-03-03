1288077ef0332c25bdc423f0df993ed9947d3733
miltonjdaz <miltonjdaz@yahoo.com>
2020-03-02 18:32:15 -0600

    Quiz program

---
+++ Python_Practice.py/Section_5/S5_Rep2.py
@@ -0,0 +1,10 @@
+""" Section 5 Rep 2 """
+
+# 
+
+def main(): 
+    letters = ["F", "D", "C", "B", "A"]
+    grade = int(input("What was the number grade (0-5): "))
+    print("The letter grade you got is:", letters[grade-1]+".")
+
+main()
