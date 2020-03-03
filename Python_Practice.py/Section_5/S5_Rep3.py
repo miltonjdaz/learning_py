31d026f1165348cb219d7273a95ba73661576ee7
miltonjdaz <miltonjdaz@yahoo.com>
2020-03-02 18:47:56 -0600

    Exam score program

---
+++ Python_Practice.py/Section_5/S5_Rep3.py
@@ -0,0 +1,11 @@
+""" Section 5 Rep 3 """ 
+
+# 100 point exam program 
+
+def main():
+    print("Exam Grader")
+    score = int(input("Enter the score (out of 100): "))
+    grades = 60*"F"+10*"D"+10*"C"+10*"B"+11*"A"
+    print("The grade is", grades[score])
+
+main()
