ca612ff1e4e81452e9384e6e0b9e357dbbe5f826
miltonjdaz <miltonjdaz@yahoo.com>
2020-06-05 09:01:47 -0500

    looking at REST API

---
+++ sra.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt 
+from github import Github 
+
+auth_token = os.environ.get('gh_token')
+# github object
+g = Github(auth_token)
+# This connects to the corresponding repository 
+repos_obj = g.get_repo('Runescape')
+import ipdb; ipdb.set_trace()
+
+"""
+def get_data(repos):
+    from github import Github
+    # Protecting the connection information 
+    auth_token = os.environ.get('gh_token')
+
+    # github object
+    g = Github(auth_token)
+    # This connects to the corresponding repository 
+    repos_obj = g.get_repo('miltonjdaz/{reposname}'.format(reposname=repos))
+    # This REST API will get the commits of the repository 
+    newobj = repos_obj.get_commits()
+    # This returns the total number of commits of the repository 
+    tc = newobj.totalCount
+    return tc
+"""
