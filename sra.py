a1b9c49261930d2ae33625069afd5d420ea9b9d7
miltonjdaz <miltonjdaz@yahoo.com>
2020-06-10 09:09:49 -0500

    Seeing the contributors

--- sra.py
+++ sra.py
@@ -12,23 +12,9 @@ clone = repos_obj.clone_url
 print(clone)
 
 des = repos_obj.description
-print = (des)
+print(des)
+
+contributor = repos_obj.get_contributors
+print(contributor)
 
 import ipdb; ipdb.set_trace()
-
-"""
-def get_data(repos):
-    from github import Github
-    # Protecting the connection information 
-    auth_token = os.environ.get('gh_token')
-
-    # github object
-    g = Github(auth_token)
-    # This connects to the corresponding repository 
-    repos_obj = g.get_repo('miltonjdaz/{reposname}'.format(reposname=repos))
-    # This REST API will get the commits of the repository 
-    newobj = repos_obj.get_commits()
-    # This returns the total number of commits of the repository 
-    tc = newobj.totalCount
-    return tc
-"""
