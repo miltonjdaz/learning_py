dee6e896cf1dcd05692b7d3bdc23ffe883ee87b5
miltonjdaz <miltonjdaz@yahoo.com>
2020-06-18 09:10:14 -0500

    Looking forward to my new job

--- Python_Practice.py/Section_10/S10_Rep3.py
+++ Python_Practice.py/Section_10/S10_Rep3.py
@@ -17,3 +17,19 @@ def getDoorPick(win, b1, b2, b3):
                     
     choiceNum = int(choice.getLabel()[-1])
     return choiceNum
+
+def updateScore(choice, msg, score, hits, misses):
+    # Compares choice to a random number
+    # Displays win or loss in msg
+    # Updates hits or misses and display of score
+    # Returns updated hits and misses
+    secret = randrange(1,4)
+    tallyStr = "Wins: {0:2}   Losses: {1:2}"
+    if choice == secret:
+        msg.setText("You win!")
+        hits = hits + 1
+    else:
+        msg.setText("You lose. The answer was door {0}.".format(secret))
+        misses = misses + 1
+    score.setText(tallyStr.format(hits,misses))
+    return hits, misses
