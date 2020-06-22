24ecb3c6b3e195b9e4ee359f0843cc467840fc72
miltonjdaz <miltonjdaz@yahoo.com>
2020-06-22 08:02:26 -0500

    GPA tracker

---
+++ Python_Practice.py/Section_10/S10_Rep4.py
@@ -0,0 +1,25 @@
+# Section 10 Rep 4 
+# Program to figure out a student's GPA
+
+class Student:
+    
+    def __init__(self, name, hours, qpoints):
+        self.name = name
+        self.hours = float(hours)
+        self.qpoints = float(qpoints)
+
+    def getName(self):
+        return self.name
+
+    def getHours(self):
+        return self.hours
+
+    def getQPoints(self):
+        return self.qpoints
+
+    def addGrade(self, gradePoint, credits):
+        self.qpoints = self.qpoints + gradePoint * credits
+        self.hours = self.hours + credits
+
+    def gpa(self):
+        return self.qpoints/self.hours
