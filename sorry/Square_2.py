384063e0710d33963ad0fb934f0f5d0d11a359b4
miltonjdaz <miltonjdaz@yahoo.com>
2021-08-03 18:57:21 -0500

    2 more

--- sorry/Square_2.py
+++ sorry/Square_2.py
@@ -6,3 +6,26 @@ def main():
     print("The temperature is", fahrenheit, "degrees Fahrenheit.")
     
 main()
+
+def dos():
+    print("This program calculates the future value of an investment.")
+    print()
+    
+    principal = eval(input("Enter the initial principal: "))
+    rate = eval(input("Enter the interest rate: "))
+    periods = eval(input("Enter the number of compounding periods per year: "))
+    years = eval(input("Enter the number of years: "))
+
+    for i in range(years * periods):
+        principal = principal * (1 + rate/periods)
+
+    print("The amount in", years, "years is:", principal)
+
+dos()
+
+def three():
+    kilometers = eval(input("How many kilometers? "))
+    miles = 0.62 * kilometers
+    print("This is the distance in miles:", miles) 
+
+three()
