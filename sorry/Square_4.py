9587fb09cc46a0e71bfc2f031c451163b8d5a5f4
miltonjdaz <miltonjdaz@yahoo.com>
2021-08-05 15:20:22 -0500

    Dates & letter

---
+++ sorry/Square_4.py
@@ -0,0 +1,26 @@
+"""def main():
+    day = int(input("Enter the day number: "))
+    month = int(input("Enter the month number: "))
+    year = int(input("Enter the year: "))
+
+    date1 = "{0}/{1}/{2}".format(month, day, year)
+
+    months = ["January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December"]
+
+    date2 = "{0} {1}, {2}".format(months[month-1],day,year)
+
+    print("The date is {0} or {1}.".format(date1, date2))
+
+main()"""
+
+def dos():
+    phrase = input("Enter a phrase: ")
+    acronym = ""
+    for word in phrase.split():
+        acronym = acronym + word[0]
+    acronym = acronym.upper()
+
+    print("The acronym is", acronym)
+
+dos()
