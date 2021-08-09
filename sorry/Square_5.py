dea74c60f5972758068120f499819e3d0b63b14a
miltonjdaz <miltonjdaz@yahoo.com>
2021-08-09 16:35:26 -0500

    song

---
+++ sorry/Square_5.py
@@ -0,0 +1,15 @@
+# My birthday is coming up 
+
+def happy():
+    print("Happy birthday to you")
+
+def sing(person):
+    happy()
+    happy()
+    print("Happy birthday, dear", person + ".")
+    happy()
+
+def main():
+    sing("Milton")
+
+main()
