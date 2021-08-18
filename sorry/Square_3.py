90327ca7ddbac22a9383fdb61acd1512ed54e491
miltonjdaz <miltonjdaz@yahoo.com>
2021-08-18 15:08:32 -0500

    Exercise

--- sorry/Square_3.py
+++ sorry/Square_3.py
@@ -19,3 +19,14 @@ def dos():
     print("The cost per square inch is:", cost_per)
 
 dos()
+
+def three():
+    hydrogen = int(input("Enter the number of hydrogen atoms: "))
+    carbon = int(input("Enter the number of carbon atoms: "))
+    oxygen = int(input("Enter the number of oxygen atoms: "))
+
+    total_weight = (hydrogen*1.00794) + (carbon*12.0107) + (oxygen*15.9994)
+
+    print("The total weight is:", total_weight)
+
+three()
