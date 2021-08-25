6841495865d658866d448cdb3e6bd1cc32e29cfc
miltonjdaz <miltonjdaz@yahoo.com>
2021-08-25 15:04:23 -0500

    Fibonacci

--- sorry/Square_7.py
+++ sorry/Square_7.py
@@ -9,3 +9,15 @@ def main():
     print("\nThe average of the numbers is", total / n)
     
 main()
+
+# Nth fibonacci with loop 
+
+def dos():
+    n = int(input("The value of n: "))
+    curr, prev = 1, 1
+    for i in range(n-2):
+        curr, prev = curr + prev, curr
+
+    print("The nth Fibonacci number is", curr)
+
+dos()
