260494c259da17e13e67d58f37aa8597a37388a7
miltonjdaz <miltonjdaz@yahoo.com>
2021-08-31 14:43:22 -0500

    safety nets

--- sorry/draft.py
+++ sorry/draft.py
@@ -13,6 +13,33 @@ DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER_ID = "" # my Spotify username 
 TOKEN = "" # my Spotify API token
 
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False 
+
+    # Primary Key Check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key check is violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # Check that all timestamps are of yesterday's date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+            raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
+
+    return True
+
 if __name__ == "__main__":
 
     # Extract part of the ETL process
