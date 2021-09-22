deed49856c369c238c4312223cc33c428f657198
miltonjdaz <miltonjdaz@yahoo.com>
2021-09-22 14:32:07 -0500

    Press Enter to quit

--- sorry/Square_2.py
+++ sorry/Square_2.py
@@ -1,48 +1,7 @@
-# Converts Celsius temps to Fahrenheit 
-
 def main():
-    celsius = eval(input("What is the Celsius temperature? "))
+    celsius = eval(input("What is the Celsius temperature"))
     fahrenheit = 9/5 * celsius + 32
-    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
-    
-    print("The program converts temperatures from Fahrenheit to Celsius")
-    print()
-    
-    fahrenheit = eval(input("What is the Fahrenheit temperature? "))
-    celsius = 5.0 / 9.0 * (fahrenheit - 32)
-    print("The temperature is", celsius, "degrees Celsius")
-    
+    print("The temp is", fahrenheit, "degrees Fahrenheit")
+    input("Press the <Enter> key to quit.")
+
 main()
-
-def dos():
-    print("This program calculates the future value of an investment.")
-    print()
-    
-    principal = eval(input("Enter the initial principal: "))
-    rate = eval(input("Enter the interest rate: "))
-    periods = eval(input("Enter the number of compounding periods per year: "))
-    years = eval(input("Enter the number of years: "))
-
-    for i in range(years * periods):
-        principal = principal * (1 + rate/periods)
-
-    print("The amount in", years, "years is:", principal)
-
-dos()
-
-def three():
-    kilometers = eval(input("How many kilometers? "))
-    miles = 0.62 * kilometers
-    print("This is the distance in miles:", miles) 
-
-three()
-
-def quad():
-    print("This program computes the average of 3 scores.")
-
-    score1, score2, score3 = eval(input("Enter 3 scores separated by a comma: "))
-    average = (score1 + score2 + score3)/ 3.0
-
-    print("The average of the scores is:", average)
-
-quad()
