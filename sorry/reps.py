3c64911db4acccb64567881c0d98e57515bf2915
miltonjdaz <miltonjdaz@yahoo.com>
2021-09-27 15:08:27 -0500

    5 loop

--- sorry/reps.py
+++ sorry/reps.py
@@ -1,7 +1,15 @@
 def main():
     score1, score2, score3 = eval(input("Enter three scores separated by a comma: "))
-    avverage = (score1 + score2 + score3) / 3.0 
+    average = (score1 + score2 + score3) / 3.0 
 
-    print(avverage)
+    print(average)
 
 main()
+
+def dos():
+    for i in range(5):
+        celsius = eval(input("What is the Celsius temperature: "))
+        fahrenheit = 9.0 / 5.0 * celsius + 32
+        print()
+
+dos()
