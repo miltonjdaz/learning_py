7bdf26ebd5bbc9323ebaede07edd49a4c2f3001c
miltonjdaz <miltonjdaz@yahoo.com>
2021-09-28 17:25:16 -0500

    brackets

--- sorry/reps.py
+++ sorry/reps.py
@@ -13,3 +13,11 @@ def dos():
         print()
 
 dos()
+
+def tres():
+    for celsius in [10,20,30,40,50,60,70,80,90]:
+        fahrenheit = 9.0 / 5.0 * celsius + 32
+        print(celsius, "        ", fahrenheit)
+    print("100         212.0")
+
+tres()
