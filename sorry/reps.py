46f1341de5fe0dec0b77bd885c92e835fe50036c
miltonjdaz <miltonjdaz@yahoo.com>
2021-10-04 14:34:15 -0500

    another method

--- sorry/reps.py
+++ sorry/reps.py
@@ -46,3 +46,16 @@ def five():
     print("The amount in", years, "years is:", principal)
 
 five()
+
+def six():
+    principal = eval(input("Enter the initial principal: "))
+    rate = eval(input("Enter the interest rate: "))
+    periods = eval(input("Enter the number of compounding periods per year: "))
+    years = eval(input("Enter the number of years: "))
+
+    for i in range(years * periods):
+        principal = principal * (1 + rate/periods)
+
+    print("The amount in", years, "years is:", principal)
+
+six()
